75bdd46faa81ee19d76db5ecbdf2c898373cef4e
Alejandro1991 <alejandroil@hotmail.com>
2015-11-28 18:12:03 +0100

    Entities and migrations added

---
+++ database/migrations/2015_11_28_165936_create_sections_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sections', function(Blueprint $table)
+        {
+            $table->increments('id');
+
+            $table->string('name_secion', 50);
+            $table->enum('status', ['visible','invisible']);
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sections');
+    }
+}
